597d9bb2d81d1e23fb0ef7805dbff055cdaec979
otanikotani <otanikotani@gmail.com>
2021-06-15 12:11:10 -0400

    SHA and BRANCH

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 echo "Hello World"
 
-SHORT_BRANCH=${GITHUB_REF#refs/*/}
-echo SHORT_BRANCH
+BRANCH=${git rev-parse --abbrev-ref HEAD}
+echo BRANCH
 SHORT_SHA=$(git rev-parse --short HEAD)
 echo SHORT_SHA
-echo "VERSION=$(if [ "$SHORT_BRANCH" = "main" ] || [ "$SHORT_BRANCH" = "master" ]; then echo `date '+%Y.%m.%d'`-$SHORT_SHA; else echo $SHORT_BRANCH-$SHORT_SHA; fi)" >> $GITHUB_ENV
+echo "VERSION=$(if [ "$BRANCH" = "main" ] || [ "$BRANCH" = "master" ]; then echo `date '+%Y.%m.%d'`-$SHORT_SHA; else echo $BRANCH-$SHORT_SHA; fi)" >> $GITHUB_ENV
