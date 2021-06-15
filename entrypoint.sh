83172d4bcde0edfb6874e330f41abaf863509e2f
otanikotani <otanikotani@gmail.com>
2021-06-15 12:12:38 -0400

    That was a missed dollar

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Hello World"
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo BRANCH
+echo $BRANCH
 SHORT_SHA=$(git rev-parse --short HEAD)
-echo SHORT_SHA
+echo $SHORT_SHA
 echo "VERSION=$(if [ "$BRANCH" = "main" ] || [ "$BRANCH" = "master" ]; then echo `date '+%Y.%m.%d'`-$SHORT_SHA; else echo $BRANCH-$SHORT_SHA; fi)" >> $GITHUB_ENV
